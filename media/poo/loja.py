6accd9c85a2d41a55d5880bcd4c9141635c1093c
Sicilia Giacomazza <102394519+Sissaz@users.noreply.github.com>
2022-06-14 13:04:20 -0300

    complete

---
+++ media/poo/loja.py
@@ -0,0 +1,70 @@
+from datetime import datetime
+
+
+Maior_Idade = 18
+
+
+class Pessoa:
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+
+    def __str__(self):
+        if not self.idade:
+            return self.nome
+        return f'{self.nome} ({self.idade} anos)'
+
+    def is_adulto(self):
+        return (self.idade or 0) > Maior_Idade
+
+
+class Cliente(Pessoa):
+    def __init__(self, nome, idade):
+        super().__init__(nome, idade)
+        self.compras = []
+
+    def registrar_compra(self, compra):
+        self.compras.append(compra)
+
+    def get_data_ultima_compra(self):
+        return None if not self.compras else \
+            sorted(self.compras, key=lambda c: c.data)[-1].data
+
+    def total_compras(self):
+        total = 0
+        for compra in self.compras:
+            total += compra.valor
+        return
+
+
+class Vendedor(Pessoa):
+    def __init__(self, nome, idade, salario):
+        super().__init__(nome, idade)
+        self.salario = salario
+
+
+class Compra:
+    def __init__(self, vendedor, data, valor):
+        self.vendedor = vendedor
+        self.data = data
+        self.valor = valor
+
+
+def main():
+    cliente = Cliente('Maria Lima', 44)
+    vendedor = Vendedor('Pedro Garrido', 36, 5000)
+    compra1 = Compra(cliente, datetime.now(), 512)
+    compra2 = Compra(cliente, datetime(2018, 6, 4), 256)
+    cliente.registrar_compra(compra1)
+    cliente.registrar_compra(compra2)
+    print(f'Cliente: {cliente}', '(adulto)' if cliente.is_adulto() else '')
+    print(f'Vendedor: {vendedor}')
+
+    valor_total = cliente.total_compras()
+    qtde_compras = len(cliente.compras)
+    print(f'Total: {valor_total} em {qtde_compras} compras')
+    print(f'Última compra: {cliente.get_data_ultima_compra()}')
+
+
+if __name__ == '__main__':
+    main()
